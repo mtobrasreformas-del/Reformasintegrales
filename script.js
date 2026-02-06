a26f37cbf8f56a9dbdc5b2a41017624ceb463389
mtobrasreformas-del <mtobrasreformas@gmail.com>
2026-02-07 00:42:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function enviarWhatsApp() {
+  var nombre = document.getElementById("nombre").value;
+  var telefono = document.getElementById("telefono").value;
+  var mensaje = document.getElementById("mensaje").value;
+
+  var texto = `Hola, soy ${nombre}. Mi teléfono es ${telefono}. 
+Necesito una reforma: ${mensaje}`;
+
+  var numero = "34600123456"; // CAMBIA TU NÚMERO
+  var url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(texto);
+
+  window.open(url, "_blank");
